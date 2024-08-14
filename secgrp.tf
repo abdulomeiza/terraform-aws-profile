ff38112ae0fa1552c69cd5cffaf7d29fef3511e4
Abdul Omeiza <abdullahiomeiza12@gmail.com>
2024-08-14 15:49:13 +0000

    commiting the security group

---
+++ secgrp.tf
@@ -0,0 +1,89 @@
+resource "aws_security_group" "profile-bean-elb-sg" {
+  name        = "profile-bean-elb-sg"
+  description = "security group for bean-elb"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "profile-bastion-sg" {
+  name        = "profile-bastion-sg"
+  description = "Security group for bastionisioner ec2 instance"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "profile_prod_sg" {
+  name        = "profile-prod-sg"
+  description = "security group for beanstalk instances"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 22
+    protocol        = "tcp"
+    to_port         = 22
+    security_groups = [aws_security_group.profile-bastion-sg.id]
+  }
+
+}
+
+resource "aws_security_group" "profile-backend-sg" {
+  name        = "profile-backend-sg"
+  description = "security group for RDS , active mq, elastic cache"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 0
+    protocol        = "-1"
+    to_port         = 0
+    security_groups = [aws_security_group.profile_prod_sg.id]
+  }
+}
+
+resource "aws_security_group_rule" "sec_group_allow_itself" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.profile-backend-sg.id
+  source_security_group_id = aws_security_group.profile-backend-sg.id
+}
