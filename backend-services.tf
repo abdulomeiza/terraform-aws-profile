a2adfcdd40827404008c5bddec2bd4e49f22f606
Abdul Omeiza <abdullahiomeiza12@gmail.com>
2024-08-20 15:13:19 +0000

    more update

--- backend-services.tf
+++ backend-services.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "profile-rds" {
   storage_type           = "gp2"
   engine                 = "mysql"
   engine_version         = "8.0.37"
-  instance_class         = "db.t2.micro"
+  instance_class         = "db.t3.micro"
   username               = var.dbuser
   password               = var.dbpass
   parameter_group_name   = "default.mysql8.0"
@@ -30,7 +30,7 @@ resource "aws_db_instance" "profile-rds" {
 resource "aws_elasticache_cluster" "profile-cache" {
   cluster_id           = "profile-cache"
   engine               = "memcached"
-  node_type            = "cache.t2.micro"
+  node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.memcached1.5"
   port                 = 11211
@@ -42,8 +42,8 @@ resource "aws_mq_broker" "profile-rmq" {
 
   broker_name        = "profile-rmq"
   engine_type        = "ActiveMQ"
-  engine_version     = "5.15.0"
-  host_instance_type = "mq.t2.micro"
+  engine_version     = "5.18.0"
+  host_instance_type = "mq.t3.micro"
   security_groups    = [aws_security_group.profile-backend-sg.id]
   subnet_ids         = [module.vpc.private_subnets[0]]
 
